cd5755facb061c0af1ba8239e431e61a1047c26a
drqueijo <silveriomat1234@gmail.com>
2026-01-02 19:22:10 -0300

    Add initial project setup with Next.js, Tailwind CSS, and component library
    - Created .gitignore to exclude unnecessary files and directories.
    - Added package.json and package-lock.json for project dependencies.
    - Configured ESLint with next.js settings in eslint.config.mjs.
    - Set up TypeScript configuration in tsconfig.json.
    - Created initial Next.js configuration in next.config.ts.
    - Added global styles in globals.css and set up Tailwind CSS.
    - Implemented basic layout structure in layout.tsx and home page in page.tsx.
    - Developed reusable UI components (Button, Input, Card, etc.) in the components/retroui directory.
    - Included chart components (AreaChart, BarChart, LineChart, PieChart) for data visualization.
    - Added utility functions for class name management in lib/utils.ts.
    - Included SVG assets in the public directory for use in the application.
    - Updated README.md with project setup instructions and deployment information.

---
+++ components/retroui/Text.tsx
@@ -0,0 +1,38 @@
+import React, { ElementType } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const textVariants = cva("font-head", {
+  variants: {
+    as: {
+      p: "font-sans text-base",
+      li: "font-sans text-base",
+      a: "font-sans text-base hover:underline underline-offset-2 decoration-primary",
+      h1: "text-4xl lg:text-5xl font-bold",
+      h2: "text-3xl lg:text-4xl font-semibold",
+      h3: "text-2xl font-medium",
+      h4: "text-xl font-normal",
+      h5: "text-lg font-normal",
+      h6: "text-base font-normal",
+    },
+  },
+  defaultVariants: {
+    as: "p",
+  },
+});
+
+type TextAs = NonNullable<VariantProps<typeof textVariants>["as"]>;
+
+type TextProps<TAs extends TextAs = "p"> = {
+  as?: TAs;
+  className?: string;
+} & Omit<React.ComponentPropsWithoutRef<TAs>, "as" | "className">;
+
+export const Text = <TAs extends TextAs = "p">(props: TextProps<TAs>) => {
+  const { className, as, ...otherProps } = props;
+  const Tag: ElementType = as as ElementType;
+
+  return (
+    <Tag className={cn(textVariants({ as }), className)} {...otherProps} />
+  );
+};
